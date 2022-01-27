b86399bc1f2f9e1d7c2715f72fea587c2c13367b
rrmmbmg <50782362+rrmmbmg@users.noreply.github.com>
2022-01-26 21:19:31 -0300

    Update suidCheck.sh

--- suidCheck.sh
+++ suidCheck.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
 echo "Localizando arquivos com permissão SUID BIT..."
-find / -perm -4000 2> /bin/null
+find / -perm -4000 2> /dev/null > list.suid
+cat list.suid
+echo "Deseja remover o SUID BIT dos arquivos? (S/N)"
+read ESCOLHA
+
+case $ESCOLHA in
+S | s)chmod -Rv -s / 2> /dev/null
+echo "Permissoes de SUID BIT removidas com sucesso!"
+sleep 3
+exit
+;;
+N | n)
+exit
+;;
+*)
+echo "Opcao invalida"
+sleep 3
+exit
+;;
+esac
